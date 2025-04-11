ba52bb0c70df11df3e0f714cb5944fa9d8eb453e
VLAD85844 <oraok@List.ru>
2025-04-11 08:57:05 +0300

    add users + product + order

--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user.urls')),
     path('', include('frontend.urls')),
+    path('products/', include('product.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
