667d0574d51f6e98492d58967ec715c07392bedd
VLAD85844 <oraok@List.ru>
2025-05-11 23:39:23 +0300

    marketplace

--- megano/order/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import OrderView, OrderDetailView, PaymentView
-
-urlpatterns = [
-    path('orders', OrderView.as_view(), name='orders'),
-    path('order/<int:order_id>', OrderDetailView.as_view(), name='order-detail'),
-    path('payment/<int:order_id>', PaymentView.as_view(), name='payment'),
-]
